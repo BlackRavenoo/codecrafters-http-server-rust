512f9d8706f2664a0514853a3ceb56a17e279225
Minnakhmetov Almaz <mastera4ek2017@gmail.com>
2024-03-08 22:24:24 +0300

    pass 2nd stage

--- src/main.rs
+++ src/main.rs
@@ -1,16 +1,22 @@
-use std::net::TcpListener;
+use std::{io::{Read, Write}, net::{TcpListener, TcpStream}};
 
 fn main() {
     let listener = TcpListener::bind("127.0.0.1:4221").unwrap();
     
     for stream in listener.incoming() {
         match stream {
-            Ok(_stream) => {
-                println!("accepted new connection");
+            Ok(stream) => {
+                handle_connection(stream);
             }
             Err(e) => {
                 println!("error: {}", e);
             }
         }
     }
+}
+
+fn handle_connection(mut stream: TcpStream) {
+    let mut buf = [0; 1024];
+    stream.read(&mut buf).unwrap();
+    stream.write("HTTP/1.1 200 OK\r\n\r\n".as_bytes()).unwrap();
 }
