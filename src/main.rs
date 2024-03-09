22ed8d5af7069b955d649f4b727932e76f3fde3f
Minnakhmetov Almaz <mastera4ek2017@gmail.com>
2024-03-09 23:50:06 +0300

    Add user-agent endpoint

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,6 @@
-use std::{io::{Read, Write}, net::{TcpListener, TcpStream}};
+use std::{collections::HashMap, io::{Read, Write}, net::{TcpListener, TcpStream}};
 
-use http_server_starter_rust::echo_response;
+use http_server_starter_rust::text_plain;
 
 fn main() {
     let listener = TcpListener::bind("127.0.0.1:4221").unwrap();
@@ -20,24 +20,44 @@ fn main() {
 fn handle_connection(mut stream: TcpStream) {
     let mut buf = [0; 1024];
     let len = stream.read(&mut buf).unwrap();
-    let args: Vec<_> = std::str::from_utf8(&buf[..len]).unwrap().split("\r\n").collect();
-    println!("{:#?}", args); //debug
-    let start_line_args = args[0].split(" ");
+    let mut args = std::str::from_utf8(&buf[..len]).unwrap().split("\r\n");
+    //println!("{:#?}", args); //debug
+    let start_line_args = args.next().unwrap().split(" ");
     let mut path = start_line_args.skip(1).next().unwrap().split("/").skip(1);
+
+    let mut headers = HashMap::new();
+    for arg in args {
+        let mut key_value = arg.split(":");
+        let key = match key_value.next() {
+            Some(key) => {
+                if key == "" {break}
+                key
+            },
+            None => {
+                eprintln!("Key was not found");
+                continue;
+            }
+        };
+        let value = match key_value.next() {
+            Some(value) => value,
+            None => {
+                eprintln!("Value was not found");
+                continue;
+            }
+        };
+        headers.insert(key, value);
+    }
+
     match path.next() {
         Some("") => {
             stream.write("HTTP/1.1 200 OK\r\n\r\n".as_bytes()).unwrap();
         },
         Some("echo") => {
-            stream.write(&echo_response(&path.collect::<Vec<&str>>().join("/"))).unwrap();
-            /*match path.next() {
-                Some(text) => {
-                    stream.write(&echo_response(text)).unwrap();
-                },
-                None => {
-                    stream.write("HTTP/1.1 400 Bad Request\r\n\r\n".as_bytes()).unwrap();
-                }
-            }*/
+            stream.write(&text_plain(&path.collect::<Vec<&str>>().join("/"))).unwrap();
+        }
+        Some("user-agent") => {
+
+            stream.write(&text_plain(headers.get("User-Agent").unwrap())).unwrap();
         }
         Some(_) => {
             stream.write("HTTP/1.1 404 Not Found\r\n\r\n".as_bytes()).unwrap();
--- src/response.rs
+++ src/response.rs
@@ -1,6 +1,6 @@
 
 
-pub fn echo_response(text: &str) -> Vec<u8> {
+pub fn text_plain(text: &str) -> Vec<u8> {
     format!(
         "HTTP/1.1 200 OK\r\nContent-Type: text/plain\r\nContent-Length: {}\r\n\r\n{}",
         text.len(),
