3f3fef6f573edefb24c32b7b7995d4708d15523f
Minnakhmetov Almaz <mastera4ek2017@gmail.com>
2024-07-31 23:19:50 +0300

    codecrafters submit [skip ci]

--- src/response.rs
+++ src/response.rs
@@ -1,23 +1,49 @@
-use std::collections::HashMap;
+use core::str;
+use std::{collections::HashMap, io::Write};
+
+use flate2::{write::GzEncoder, Compression};
+
+enum Encoding {
+    Gzip,
+    None
+}
 
 pub fn response(content_type: &str, headers: &HashMap<&str, &str>, text: &str) -> Vec<u8> {
+    let mut encoding = Encoding::None;
     let headers = headers.iter().fold(HashMap::new(), |mut acc, (key, val)| {
         match *key {
             "Accept-Encoding" if val.split(',').any(|encoding| encoding.trim() == "gzip") => {
                 acc.insert("Content-Encoding", "gzip");
+                encoding = Encoding::Gzip;
             }
             _ => ()
         }
         acc
     });
-    format!(
-        "HTTP/1.1 200 OK\r\n{}Content-Type: {}\r\nContent-Length: {}\r\n\r\n{}",
+
+    let mut result = format!(
+        "HTTP/1.1 200 OK\r\n{}Content-Type: {}\r\n",
         headers.iter().fold(String::new(), |acc, (key, val)| {
             acc + key + ": " + val + "\r\n"
           }
         ),
         content_type,
-        text.len(),
-        text
-    ).as_bytes().to_vec()
+    ).as_bytes().to_vec();
+
+    match encoding {
+        Encoding::Gzip => {
+            let mut encoder = GzEncoder::new(Vec::new(), Compression::default());
+            let _ = encoder.write_all(text.as_bytes());
+            let text = encoder.finish().unwrap();
+            
+            result.extend_from_slice(format!("Content-Length: {}\r\n\r\n", text.len()).as_bytes());
+            result.extend_from_slice(&text)
+        },
+        Encoding::None => {
+            result.extend_from_slice(format!("Content-Length: {}\r\n\r\n", text.len()).as_bytes());
+            result.extend_from_slice(text.as_bytes())
+        },
+    }
+
+    result
 }
